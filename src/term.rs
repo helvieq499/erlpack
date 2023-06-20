391dbe3c33c4a908df14691086add4406de964f5
helvieq499 <73959954+helvieq499@users.noreply.github.com>
2023-06-19 21:56:18 -0400

    add erlang types

--- src/term.rs
+++ src/term.rs
@@ -1,1 +1,12 @@
-pub enum Term {}
+use std::collections::HashMap;
+
+pub enum Term {
+    Integer(Vec<u8>),
+    Float(f64),
+    Atom(String),
+    Tuple(Vec<Term>),
+    Map(HashMap<Term, Term>),
+    Nil,
+    List(Vec<Term>, Option<Box<Term>>),
+    Binary(Vec<u8>),
+}
